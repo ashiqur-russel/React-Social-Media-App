bbc1b4a68750996b90379cb133396b1b0554df0c
ashiqurras <ashiqurrussel2@gmail.com>
2022-01-31 00:48:43 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './pages/home/Home';
+
+const App = () => {
+  return <div><Home /></div>;
+};
+
+export default App;
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopBar from "../../components/topbar/TopBar";
+
+
+export default function Home() {
+    return (
+        <>
+            <TopBar />
+        </>
+
+    );
+}
