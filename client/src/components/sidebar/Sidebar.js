b5efc7779b74920bd040b1f31ff90dd39381250b
ashiqurras <ashiqurrussel2@gmail.com>
2022-01-31 01:49:51 +0100

    sidebar

---
+++ client/src/components/sidebar/Sidebar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import "./sidebar.css"
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import ChatIcon from '@mui/icons-material/Chat';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import GroupIcon from '@mui/icons-material/Group';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import EventIcon from '@mui/icons-material/Event';
+import SchoolIcon from '@mui/icons-material/School';
+
+
+
+
+export default function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className="sidebarWrapper">
+                <ul className="siderbarList">
+                    <li className="sidebarListItem">
+                        <RssFeedIcon className='sidebarIcon' />
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <ChatIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PlayCircleOutlineIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <GroupIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <BookmarkIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <HelpOutlineIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <WorkOutlineIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Jobs</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <EventIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Events</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <SchoolIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Courses</span>
+                    </li>
+                </ul>
+                <button className='sidebarButton'>Show More</button>
+                <hr className='sidebarHr' />
+                <ul className="sidebarFriendList">
+                    <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImage" />
+                    <span className="sidebarFriendName">Jane Doe</span>
+                </ul>
+            </div>
+
+        </div>
+    );
+}
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
 import TopBar from "../../components/topbar/TopBar";
+import Sidebar from "../../components/sidebar/SideBar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+
+import "./home.css"
+
 
 
 export default function Home() {
     return (
         <>
             <TopBar />
+            <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+                <Rightbar />
+
+            </div>
         </>
 
     );
